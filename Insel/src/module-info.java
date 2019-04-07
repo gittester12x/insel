9b4f47444f0b69b4185ec30e83898e04377576a9
gittester12x <paul.schal@gmx.de>
2019-04-07 13:26:33 +0200

    This is a test project, shared with git.

---
+++ Insel/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author paul
+ *
+ */
+module insel {
+}
---
+++ Insel/src/squared/Squared.java
@@ -0,0 +1,22 @@
+package squared;
+
+public class Squared {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double[] array = {1.24,4.54,4,77,9.89};
+		System.out.println("Der Durchschnitt ist: " + avg(array) );
+	}
+
+	static double avg( double[] array ) {
+		if( array == null || array.length == 0 )
+			throw new IllegalArgumentException( "Array null oder leer");
+		
+		double sum = 0;
+		
+		for( double n : array )
+			sum+= n;
+		
+		return sum;
+	}
+}
